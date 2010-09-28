f16437e3d8c3c36c955cc3c0c0fbfe37fa1c01f0
Justin Lynn <j@3crowd.com>
2010-09-27 19:19:08 -0700

    initial commit

---
+++ lib/apc-report-parser.rb
@@ -0,0 +1,57 @@
+#!/usr/bin/env ruby
+require 'rubygems'
+require 'uri'
+require 'net/http'
+require 'nokogiri'
+
+class ReportNotInitializedError < StandardError
+end
+
+# The loader and parser should be factored out, but this is good enough for now
+class APCStatsReport
+
+  def initialize(uri="http://localhost:80/apc.php", lazy_load=true)
+    @uri = URI.parse(uri)
+    @lazy_load = lazy_load
+  end
+
+  def retrieve_report!
+    resource = Net::HTTP.start(@uri.host, @uri.port) do |http|
+      http.get(@uri.path)
+    end
+    @report = parse_resource_into_report(resource)
+  end
+
+  def report
+    if @report.nil?
+      raise ReportNotInitializedError unless @lazy_load
+      retrieve_report!
+    end
+    @report
+  end
+
+  private
+
+  def parse_resource_into_report(resource)
+    document = Nokogiri::HTML(resource.body)
+    headers = extract_headers(document)
+    report = headers.inject(Hash.new) do |results, header|
+      results[header.text] = extract_substats(header)
+      results
+    end
+    return report
+  end
+
+  def extract_headers(document)
+    document.css('body div.content div.info h2')
+  end
+
+  def extract_substats(header)
+    header.parent.children.css('tr').inject(Hash.new) do |substats, row|
+      label = row.children[0]
+      data = row.children[1]
+      substats[label.text] = data.text
+      substats
+    end
+  end
+end
